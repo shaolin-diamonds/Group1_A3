faedcb818405726e64a70fd970f0f3e0e18fe532
GMarinBorges <75048870+GMarinBorges@users.noreply.github.com>
2022-10-20 18:28:34 -0500

    Update dylanspage.dart

--- group1a3/lib/dylansfolder/dylanspage.dart
+++ group1a3/lib/dylansfolder/dylanspage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
 
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
+class dylanspage extends StatelessWidget {
+  const dylanspage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +12,25 @@ class Page2 extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              "I like to build and paint modal kits.",
-              style: TextStyle(fontSize: 25.0),
-            ),
-            ElevatedButton(
-                onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyHomePage(title: 'main'))),
-                child: const Text("GOTO MAIN"))
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              children: <Widget>[
+                const Text(
+                  "I like to build and paint modal kits.",
+                  style: TextStyle(fontSize: 25.0),
+                ),
+                ElevatedButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const MyHomePage(title: 'main'))),
+                    child: const Text("GOTO MAIN",
+                        style: TextStyle(fontSize: 21, color: Colors.black)))
+              ],
+            )),
       ),
     );
   }
