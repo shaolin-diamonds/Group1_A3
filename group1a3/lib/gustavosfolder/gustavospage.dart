181fcdacfc1b5cbbe54bcc57ccc9213f274501d5
GMarinBorges <75048870+GMarinBorges@users.noreply.github.com>
2022-10-20 00:44:04 -0500

    Update gustavospage.dart
    gustavospage

--- group1a3/lib/gustavosfolder/gustavospage.dart
+++ group1a3/lib/gustavosfolder/gustavospage.dart
@@ -10,49 +10,5 @@ import '../main.dart';
 */
 
 class gustavospage extends StatelessWidget {
-  //key that identifies the view
-  const gustavospage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gustavo\'s Page', style: TextStyle(fontSize: 25)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                //Under title lable
-                '1. I am venezuelan.\n' +
-                    '2. I collect videogames.\n' +
-                    '3. My favorite show is Shaman King.',
-                //Font size
-                style: TextStyle(fontSize: 21),
-              ),
-            ),
-            //Button
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: ElevatedButton(
-                  //takes you back to main page
-                  child: Text("GOTO Main",
-                      style: TextStyle(fontSize: 21, color: Colors.white)),
-                  style: TextButton.styleFrom(backgroundColor: Colors.red),
-                  onPressed: () {
-                    //Routes you to main.dart
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const MyApp()),
-                    );
-                  }),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
