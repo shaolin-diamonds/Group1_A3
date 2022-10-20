d8aa42658a385e15bf1f8edc082c0ac440fffeb5
GMarinBorges <75048870+GMarinBorges@users.noreply.github.com>
2022-10-20 00:24:47 -0500

    Add files via upload
    main and gustavospage

--- group1a3/lib/main.dart
+++ group1a3/lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'gustavosfolder/gustavospage.dart';
+
+/*
+  Authors:
+    Anthony Huisenga-Wills​
+    Dylan Feddersen​
+    Chevy Rios​
+    Gustavo Marin Borges
+  Date: 10/13/2022
+  Title: Group Assignment 3
+  Class: CMSC 2204 – Mobile Device Programming
+*/
 
 void main() {
   runApp(const MyApp());
@@ -13,33 +25,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Group 1 Github Application'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +42,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                //Title outside bar
+                'Group 1 Application',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            //First Button
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  child: Text("Chevy\'s Page",
+                      style: TextStyle(fontSize: 21, color: Colors.white)),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.purple, fixedSize: Size(200, 50)),
+                  onPressed: (() => setState(() {
+                        //Routes you to chevyspage.dart
+                        /*Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const chevyspage()),
+                        );*/
+                      }))),
+            ),
+            //Second button
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  child: Text("Dylan\'s Page",
+                      style: TextStyle(fontSize: 21, color: Colors.white)),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue, fixedSize: Size(200, 50)),
+                  onPressed: (() => setState(() {
+                        //Routes you to dylanspage.dart
+                        /*Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const dylanspage()),
+                        );*/
+                      }))),
+            ),
+            //Third button
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  child: Text(
+                    "Anthony\'s Page",
+                    style: TextStyle(fontSize: 21, color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.black, fixedSize: Size(200, 50)),
+                  onPressed: (() => setState(() {
+                        //Routes you to anthonyspage.dart
+                        /*Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const anthonyspage()),
+                        );*/
+                      }))),
+            ),
+            //Fourth button
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  child: Text("Gustavo\'s Page",
+                      style: TextStyle(fontSize: 21, color: Colors.white)),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.red, fixedSize: Size(200, 50)),
+                  onPressed: (() => setState(() {
+                        //Routes you to gustavospage.dart
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const gustavospage()),
+                        );
+                      }))),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
